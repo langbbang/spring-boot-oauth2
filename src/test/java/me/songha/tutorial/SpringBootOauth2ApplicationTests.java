b326077a2ef4dd201d4a273f3cba8e85838d3b78
songhashin <lang_00@naver.com>
2023-01-08 13:38:06 +0900

    init

---
+++ src/test/java/me/songha/tutorial/SpringBootOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.songha.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
